58adc77decb54ed9ea6fa808e286a3f0a9c1c00a
Cemil <cemil.residovski@opticon.se>
2024-07-10 14:14:13 +0200

    added reqs to html box

--- html_boxes.py
+++ html_boxes.py
@@ -1,9 +1,9 @@
 def result(background_color, color):
     result = f"""<div style='background-color:{background_color};color:{color};
     margin-left:auto;margin-right:auto;text-align:center;vertical-align:middle;'>
-    <p style='font-size:24px;'>Large text</p>
-    <p style='font-size:18.5px;font-weight:bold;'>Large text</p>
-    <p style="font-size:18.5px;">Normal text</p></div>"""
+    <p style='font-size:24px;'>Large text (AA)</p>
+    <p style='font-size:18.5px;font-weight:bold;'>Large text (AA)</p>
+    <p style="font-size:18.5px;">Normal text (AAA)</p></div>"""
     return result
 
 
